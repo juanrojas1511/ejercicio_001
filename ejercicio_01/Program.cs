c1c11f8e713ed399d8df05fa5f4240649d4a25c7
Juan Paris <juanpajares6@gmail.com>
2023-10-04 21:55:37 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n:");
+            int valor_num = int.Parse(Console.ReadLine());
+            double resultado = 0;
+            double suma = 0;
+            for (int i = 1; i <= valor_num; i++)
+            {
+                resultado = (16.0 - i) / (3.0 * i);
+                Console.WriteLine($"resultado:{resultado}");
+                suma += resultado;
+            }
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+    }
+}
